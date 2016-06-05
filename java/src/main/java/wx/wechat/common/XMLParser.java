34e72f8554cc9f6bf854a2ede0a4eef0c431a35d
wxyyxc1992 <hoteam@live-forest.com>
2016-06-05 22:41:15 +0800

    Initial Commit

---
+++ java/src/main/java/wx/wechat/common/XMLParser.java
@@ -0,0 +1,12 @@
+package wx.wechat.common;
+
+import java.util.Map;
+
+/**
+ * Created by apple on 16/6/5.
+ */
+public class XMLParser {
+    public static Map<String, Object> getMapFromXML(String responseString) {
+        return null;
+    }
+}
