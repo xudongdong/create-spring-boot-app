8506f2254f8b1c2b27a0e388f09a2d5adee9ab7d
张梓雄 <zhangzixiong@1001hao.com>
2017-03-23 23:03:09 +0800

    添加初始化配置文件

---
+++ gradle-boilerplate/module/api/src/main/java/wx.csba/controller/IndexController.java
@@ -0,0 +1,21 @@
+package wx.csba.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public String greeting(
+            @RequestParam(value = "name", defaultValue = "World") String name) {
+        return counter.incrementAndGet() +
+                String.format(template, name);
+    }
+}
